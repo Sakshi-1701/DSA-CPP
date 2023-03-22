3e2ef1275ee1d5d891be85f74a3b5be8f33ac7b5
Sakshi Vashishtha <84696772+Sakshi-1701@users.noreply.github.com>
2023-03-23 00:06:11 +0530

    Create rotate_array.cpp

---
+++ Arrays/rotate_array.cpp
@@ -0,0 +1,15 @@
+//https://leetcode.com/problems/rotate-array/description/
+
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n=nums.size();
+        k=k%n;    //since k can be greater than the array too
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.begin()+k);
+        reverse(nums.begin()+k,nums.end());
+        
+         
+    }
+};
